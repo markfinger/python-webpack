e431af72068a94105c704ea95f06c419488fbff2
Mark Finger <markfinger@gmail.com>
2015-01-26 19:58:29 +1100

    Bumping the django-node version to 2.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     },
     install_requires=[
         'django',
-        'django-node == 2.2.0',
+        'django-node == 2.2.1',
     ],
     description='Generate Webpack bundles from a Django application.',
     long_description='Documentation at https://github.com/markfinger/django-webpack',
