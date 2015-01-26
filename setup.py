63e155a20ab9ebd32f3006ea8d18c724cb88eee3
Mark Finger <markfinger@gmail.com>
2015-01-26 19:49:16 +1100

    Updated django-node to latest bug fix release.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     },
     install_requires=[
         'django',
-        'django-node >= 2.1.0',
+        'django-node == 2.2.0',
     ],
     description='Generate Webpack bundles from a Django application.',
     long_description='Documentation at https://github.com/markfinger/django-webpack',
