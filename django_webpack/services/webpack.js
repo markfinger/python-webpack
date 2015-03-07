8f634c8643688cb847de967125ffcf0279618e17
Mark Finger <markfinger@gmail.com>
2015-03-07 11:10:03 +1100

    Bundles are now cached at the service layer. Multiple pending requests for the same bundle will now await until a single compilation process completes.
    Fixes #9

--- django_webpack/services/webpack.js
+++ django_webpack/services/webpack.js
@@ -1,7 +1,34 @@
 var webpack = require('webpack');
 var WebpackWatcher = require('webpack-watcher');
 
-var errorResponse = function(response, error) {
+var bundles = {};
+
+var getBundle = function(options) {
+	if (bundles[options.pathToConfig] === undefined) {
+		bundles[options.pathToConfig] = {
+			pathToConfig: options.pathToConfig,
+			config: null,
+			output: null,
+			generatingBundle: true,
+			pendingResponses: []
+		};
+	}
+	return bundles[options.pathToConfig];
+};
+
+var sendBundleResponses = function(bundle) {
+	var pendingResponses = bundle.pendingResponses;
+	bundle.pendingResponses = [];
+
+	var output = bundle.output;
+	var jsonOutput = JSON.stringify(output);
+
+	pendingResponses.forEach(function(response) {
+		response.send(jsonOutput);
+	});
+};
+
+var sendErrorResponse = function(response, error) {
 	if (error instanceof Error) {
 		console.error(error);
 	} else {
@@ -10,61 +37,81 @@ var errorResponse = function(response, error) {
 	response.status(500).send(error);
 };
 
-var processConfig = function(request, config) {
-	var bundleRoot = request.query.bundle_root;
-	if (!bundleRoot) {
-		return errorResponse(response, 'No bundle_root option was provided');
+var getConfig = function(options, response) {
+	var bundle = getBundle(options);
+
+	if (!bundle.config || !options.cacheConfigFiles) {
+		// Ensure that config files are not cached in development
+		if (options.pathToConfig in require.cache) {
+			delete require.cache[options.pathToConfig];
+		}
+		try {
+			var config = require(options.pathToConfig);
+		} catch(e) {
+			return sendErrorResponse(response, e);
+		}
+		// Ensure that the config exports something
+		if (config === undefined) {
+			return sendErrorResponse(response, 'Config file "' + options.pathToConfig + '" exports undefined.');
+		}
+		if (config.output && config.output.path) {
+			config.output.path = config.output.path.replace('{{ BUNDLE_ROOT }}', options.bundleRoot);
+		}
+		bundle.config = config;
 	}
-	if (config.output && config.output.path) {
-		config.output.path = config.output.path.replace('{{ BUNDLE_ROOT }}', bundleRoot);
-	}
+
 	return config;
 };
 
-var generateBundle = function(request, response, pathToConfig) {
-	try {
-		var config = require(pathToConfig);
-	} catch(e) {
-		return errorResponse(response, e);
+var generateBundle = function(options, response) {
+	var bundle = getBundle(options);
+	bundle.pendingResponses.push(response);
+
+	if (!bundle.generatingBundle) {
+		return sendBundleResponses(bundle);
 	}
 
-	if (config === undefined) {
-		return errorResponse(response, 'Config file "' + pathToConfig + '" exports undefined.');
-	}
-
-	config = processConfig(request, config);
-
+	var config = getConfig(options, response);
 	webpack(config, function(err, stats) {
 		if (err) {
 			console.error(new Error(err));
 			response.status(500).send(err);
+			bundle.error = err;
 			return;
+		} else {
+			bundle.error = null;
 		}
 		// TODO: pass out the assets, errors, and warnings, the stats toJson object has too much data in it
 		// TODO: limit the json output: http://webpack.github.io/docs/node.js-api.html#stats-tojson
-		var output = {
+		bundle.output = {
 			config: config,
 			stats: stats.toJson()
 		};
-		response.send(JSON.stringify(output));
+		bundle.generatingBundle = false;
+		sendBundleResponses(bundle);
 	});
 };
 
-var service = function(request, response) {
+var service = function service(request, response) {
 	var pathToConfig = request.query.path_to_config;
 
-	// Ensure that config files are not cached during development
-	var cache = request.query.cache === 'True';
-	if (!cache && pathToConfig in require.cache) {
-		delete require.cache[pathToConfig];
+	if (!request.query.cache_config_files === undefined) {
+		return sendErrorResponse(response, 'No cache_config_files option was provided');
+	}
+	var cacheConfigFiles = request.query.cache_config_files === 'True';
+
+	var bundleRoot = request.query.bundle_root;
+	if (!bundleRoot) {
+		return sendErrorResponse(response, 'No bundle_root option was provided');
 	}
 
-	//if (cache) {
-		generateBundle(request, response, pathToConfig);
-	//} else {
-	//
-	//}
+	var options = {
+		cacheConfigFiles: cacheConfigFiles,
+		bundleRoot: bundleRoot,
+		pathToConfig: pathToConfig
+	};
 
+	generateBundle(options, response);
 };
 
 module.exports = service;
--- django_webpack/settings.py
+++ django_webpack/settings.py
@@ -11,4 +11,12 @@ BUNDLE_URL = setting_overrides.get('BUNDLE_URL', None)
 if BUNDLE_URL is None:
     raise ImproperlyConfigured('DJANGO_WEBPACK[\'BUNDLE_URL\'] has not been defined')
 
-CACHE = setting_overrides.get('CACHE', not settings.DEBUG)
+CACHE = setting_overrides.get(
+    'CACHE',
+    not settings.DEBUG
+)
+
+CACHE_CONFIG_FILES = setting_overrides.get(
+    'CACHE_CONFIG_FILES',
+    not settings.DEBUG
+)
