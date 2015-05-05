c4ad1df3d606eab5ee32ec4749e1c51f218fb996
Mark Finger <markfinger@gmail.com>
2015-05-05 23:02:49 +1000

    Added deprecation notice for django-webpack

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ setup(
         'optional-django==0.1.0'
     ],
     description='Generate Webpack bundles from a Django application.',
-    long_description='Documentation at https://github.com/markfinger/django-webpack',
+    long_description='''
+django-webpack is now deprecated and has been replaced by https://github.com/markfinger/python-webpack
+
+Documentation for django-webpack is available at https://github.com/markfinger/python-webpack/tree/3.1.0
+''',
     author='Mark Finger',
     author_email='markfinger@gmail.com',
     url='https://github.com/markfinger/django-webpack',
