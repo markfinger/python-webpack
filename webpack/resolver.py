9441fa2d48bedc8253a8af43ea851b127bb6973c
Mark Finger <markfinger@gmail.com>
2015-06-26 12:26:07 +1000

    Improved the clarity of the config file lookup errors.

--- webpack/resolver.py
+++ webpack/resolver.py
@@ -8,13 +8,18 @@ def find_config_file(config_file):
     if not os.path.isabs(config_file):
         if settings.CONFIG_DIRS:
             for dir_path in settings.CONFIG_DIRS:
-                path = os.path.join(dir_path, config_file)
-                if os.path.exists(path):
-                    return path
+                abs_path = os.path.join(dir_path, config_file)
+                if os.path.exists(abs_path):
+                    return abs_path
 
         abs_path = staticfiles.find(config_file)
         if not abs_path or not os.path.exists(abs_path):
-            raise ConfigFileNotFound(config_file)
+            config_dirs = ''
+            if settings.CONFIG_DIRS:
+                config_dirs += ' tried: '
+                for dir_path in settings.CONFIG_DIRS:
+                    config_dirs += os.path.join(dir_path, config_file)
+            raise ConfigFileNotFound('{}{}'.format(config_file, config_dirs))
 
         return abs_path
 
